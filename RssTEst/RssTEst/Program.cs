e959b330ebcc861121716a5e9123d19db4637c37
Antoine Bichon <antoinebichon@live.fr>
2014-01-07 22:39:46 +0100

    init de la db avec une table Flux

--- RssTEst/RssTEst/Program.cs
+++ RssTEst/RssTEst/Program.cs
@@ -13,7 +13,21 @@ namespace RssTEst
     {
         private static void Main(string[] args)
         {
-            SQLiteConnection.CreateFile("DBTest.sqlite");
+            using (var sqliteConn = new SQLiteConnection("Data Source=DBTest.sqlite;Version=3;"))
+            {
+                sqliteConn.Open();
+                string requete =
+                    "create table Flux (id int(10), text varchar(2048), title varchar(2048), image varchar (2048), link varchar(2048), date Datetime) ";
+                var sqlLiteComm = new SQLiteCommand(requete, sqliteConn);
+                sqlLiteComm.ExecuteNonQuery();
+
+                var comtest =
+                    "insert into Flux values (1,'Je test le contenu d''un flux lolololil les sarrasins sont dans le champs de blé, je repete lol kikoo','Titre !','http://www.google.fr/img','http://www.google.fr',date('now'))";
+                sqlLiteComm = new SQLiteCommand(comtest,sqliteConn);
+                sqlLiteComm.ExecuteNonQuery();
+                sqliteConn.Close();
+
+            }
             
         }
     }
