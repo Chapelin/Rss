177c0d5e176aa2fe1f804bfe4cabe00ee09b435b
Antoine Bichon <antoinebichon@live.fr>
2014-01-07 22:17:13 +0100

    Preparation archi Service/DB/Web

--- RssTEst/RssTEst/Program.cs
+++ RssTEst/RssTEst/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.ServiceModel.Syndication;
 using System.Xml;
+using System.Data.SQLite;
 
 namespace RssTEst
 {
@@ -12,19 +13,7 @@ namespace RssTEst
     {
         private static void Main(string[] args)
         {
-            WebClient client = new WebClient();
-            client.Proxy = WebRequest.DefaultWebProxy;
-            client.Proxy.Credentials = CredentialCache.DefaultCredentials;
-            client.Credentials = CredentialCache.DefaultCredentials;
-
-
-                // Read the feed using an XmlReader  
-                using (XmlReader reader = XmlReader.Create(client.OpenRead("http://rss.lemonde.fr/c/205/f/3050/index.rss")))
-                {
-                    var feed = SyndicationFeed.Load(reader);
-                    Console.WriteLine(feed.Id);
-
-                }
+            SQLiteConnection.CreateFile("DBTest.sqlite");
             
         }
     }
