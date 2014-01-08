a8ee0ab5f59658a44993c5082a357b013354385b
Antoine Bichon <antoine.bichon@cgi.com>
2014-01-08 13:30:05 +0100

    DEbut logique not working

---
+++ RssTEst/ServiceRssToDB/Flux.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceRssToDB
+{
+    public class Flux
+    {
+        public int ID { get; set; }
+        public string text { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string link { get; set; }
+        public DateTime date { get; set; }
+
+
+
+
+        public string ToRequest()
+        {
+            return "insert into Flux values (" + ID + ", '" + text.Replace("'", "''") + "','" + title.Replace("'", "''") +
+                   "','" + image.Replace("'", "''") + "','" + link.Replace("'", "''") + "','" +
+                   date.ToString("yy-MM-dd HH:mm:ss") + "');";
+        }
+    }
+}
--- RssTEst/ServiceRssToDB/Program.cs
+++ RssTEst/ServiceRssToDB/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
 using System.Text;
+using System.Xml;
 
 namespace ServiceRssToDB
 {
@@ -9,6 +13,45 @@ namespace ServiceRssToDB
     {
         static void Main(string[] args)
         {
+
+            SyndicationFeed feed;
+            WebClient client = new WebClient();
+            client.Proxy = WebRequest.DefaultWebProxy;
+            client.Proxy.Credentials = CredentialCache.DefaultCredentials;
+            client.Credentials = CredentialCache.DefaultCredentials;
+
+
+            // Read the feed using an XmlReader  
+            using (XmlReader reader = XmlReader.Create(client.OpenRead("http://rss.lemonde.fr/c/205/f/3052/index.rss")))
+            {
+                var liste = new List<Flux>();
+                feed = SyndicationFeed.Load(reader);
+                foreach (var elem in feed.Items)
+                {
+                    var temp = new Flux()
+                    {
+                        date = elem.PublishDate.DateTime,
+                        ID = 1,
+                        image = elem.Links[1].GetAbsoluteUri().ToString(),
+                        link = elem.Links[0].GetAbsoluteUri().ToString(),
+                        title = elem.Title.Text,
+                        text = elem.Summary.Text
+
+                    };
+                    liste.Add(temp);
+                }
+                using (var sqliteConn = new SQLiteConnection("Data Source=DBTest.sqlite;Version=3;"))
+                {
+                    sqliteConn.Open();
+                    foreach (var flux in liste)
+                    {
+                        new SQLiteCommand(flux.ToRequest(), sqliteConn).ExecuteNonQuery();
+                    }
+                    sqliteConn.Close();
+             
+                }
+
+            }
         }
     }
 }
