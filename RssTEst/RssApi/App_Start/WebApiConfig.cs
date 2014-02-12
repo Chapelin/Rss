ed96f6b60cbf50d9769b0d90547d23fab611e0a2
Antoine Bichon <antoine.bichon@cgi.com>
2014-02-12 13:53:19 +0100

    on force les reponses webapi en json

--- RssTEst/RssApi/App_Start/WebApiConfig.cs
+++ RssTEst/RssApi/App_Start/WebApiConfig.cs
@@ -18,6 +18,10 @@ namespace RssApi
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new {id = RouteParameter.Optional});
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
+
     }
 }
