ddc8f63507a6ab12cdedb08fdd91d5e4ccc95b15
Antoine Bichon <antoinebichon@live.fr>
2014-02-12 20:06:21 +0100

    Ajout affichage grossier par 3

--- RssTEst/RssApi/Scripts/_custom/rss.js
+++ RssTEst/RssApi/Scripts/_custom/rss.js
@@ -1,7 +1,8 @@
 ﻿
 $(document).ready(function() {
     console.log("ready §§");
-    RequeteLast20("52f8be4f46485e02dc1edd2d");
+//    RequeteLast20("52f8be4f46485e02dc1edd2d");
+    RequeteLast20("52f90d1031d5190e84aa8e65");
 });
 function RequeteLast20(source) {
     console.log("Lancement appel");
@@ -16,10 +17,27 @@ function AfficherDonnes(data) {
     var items = [];
     console.log("Reponse ok");
     console.log(data);
-    $.each(data, function(key, val) {
-        items.push(val.Titre);
+    $.each(data, function (key, val) {
+        items.push(val);
     });
-    console.log("Test : "+$("#test"));
-    $("#test").html(items.join("<br\>"));
+    
+    var total = '';
+    for (var i = 0; i < items.length; i++) {
+        if(i%3===0) {
+            total += '<div class="row">';
+        }
+
+        total += '<div class="col-lg-3">';
+        total += '<h2>' + items[i].Titre+'</h2>';
+        total += '<p>' + items[i].Texte+'</p>';
+        total += '</div>';
+
+
+        if(i%3===2) {
+            total += '</div>';
+        }
+    }
+    console.log("Total : " + total);
+    $("#test").html(total);
 
 }
