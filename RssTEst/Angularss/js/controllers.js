34e69c22f1be584bd82a4cdaca5e4188077263b7
Antoine Bichon <antoine.bichon@cgi.com>
2014-02-27 13:44:06 +0100

    integration premiere page + code associé

--- RssTEst/Angularss/js/controllers.js
+++ RssTEst/Angularss/js/controllers.js
@@ -1,3 +1,23 @@
 'use strict';
 
 /* Controllers */
+var baseRssModule = angular.module('baseRssModule',[]);
+
+
+baseRssModule.controller('baseRssController', function($scope,$http) {
+  $scope.test = "test";
+    $http.get('http://localhost:10793/api/entree/getlast').success(function(data) {
+    	$scope.Rss = data;
+    	console.log("reponse");
+  }).error(function(data, status, headers, config){
+  	console.log(data);
+  		console.log(status);
+
+console.log(headers);
+
+console.log(config);
+
+
+
+  	});
+});
