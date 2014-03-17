3633a7f05f225e1f1e8c5090f531b8afd35a4886
Antoine Bichon <antoine.bichon@cgi.com>
2014-03-17 13:55:22 +0100

    avancement site angular : attention auc routes ?

--- RssTEst/Angularss/js/app.js
+++ RssTEst/Angularss/js/app.js
@@ -1,3 +1,34 @@
 'use strict';
 
 /* App Module */
+var rssApp = angular.module('rssApp',
+['ngRoute',
+  'baseRssController']);
+
+
+rssApp.config(['$routeProvider',
+	function($routeProvider)
+	{
+		$routeProvider.
+			when('/index',
+				{
+					 templateUrl: 'index.html',
+						controller: 'RssController'
+				}).
+			when('/Categories',
+				{
+						 templateUrl: 'partial/categories.html',
+						controller: 'CategoriesController'
+				}).
+			when('/Sources',
+				{
+						 templateUrl: 'partial/sources.html',
+						controller: 'SourcesController'
+				}).
+			when('/Source/:sourceId',
+				{
+						 templateUrl: 'partial/source.html',
+						controller: 'SpecificSourceController'
+				}).
+			otherwise({ redirectTo : "/index"});
+	}]);
--- RssTEst/Angularss/js/controllers.js
+++ RssTEst/Angularss/js/controllers.js
@@ -1,24 +1,30 @@
 'use strict';
 
 /* Controllers */
-var baseRssModule = angular.module('baseRssModule',[]);
+var baseRssController = angular.module('baseRssController',[]);
 
 
-baseRssModule.controller('baseRssController', function($scope,$http) {
-  $scope.test = "test";
+baseRssController.controller('RssController', function($scope,$http) {
+  $scope.test = "RssController";
     $http.get('http://localhost:5555/Entrees/getlastX').success(function(data) {
     	console.log("reponse : "+data);
     	$scope.Rss = data;
-    	
   }).error(function(data, status, headers, config){
   	console.log("error : "+data);
   		console.log(status);
-
-console.log(headers);
-
-console.log(config);
+  	});
+});
 
 
 
+
+baseRssController.controller('CategoriesController', function($scope,$http) {
+  $scope.test = "CategoriesController";
+    $http.get('http://localhost:5555//Categories/GetAll').success(function(data) {
+    	console.log("reponse : "+data);
+    	$scope.Categories = data;
+  }).error(function(data, status, headers, config){
+  	console.log("error : "+data);
+  		console.log(status);
   	});
 });
