28eaa76cd7e9d98245f1ad544765d91f3ce08860
Antoine Bichon <antoine.bichon@cgi.com>
2014-03-20 12:19:24 +0100

    ajout suppression GridFs lors du clean + bool à renseigner si environnement limité niveau requetes

--- RssTEst/RssTEst/Program.cs
+++ RssTEst/RssTEst/Program.cs
@@ -15,12 +15,13 @@ namespace RssTEst
 
             try
             {
+                bool isLimited = true;
                 Console.WriteLine("Go !");
-                //Clean();
-                //InitSources();
-                //InitCategories();
-                //InitCategoriesSources();
-                //InitIndex();
+                Clean();
+                InitSources(isLimited);
+                InitCategories();
+                InitCategoriesSources();
+                InitIndex();
                 //DlImage("53049aad46485e268c6e14eb");
             }
             catch (Exception e)
@@ -39,6 +40,8 @@ namespace RssTEst
 
 
         }
+
+
 
         private static void DlImage(string id)
         {
@@ -65,6 +68,9 @@ namespace RssTEst
             DBManager.Entrees.Drop();
             DBManager.Categories.Drop();
             DBManager.Sources.Drop();
+            DBManager.Favicon.Drop();
+            DBManager.GridFS.Chunks.RemoveAll();
+            DBManager.GridFS.Files.RemoveAll();
             Console.WriteLine("contenu droppé");
         }
 
@@ -113,67 +119,109 @@ namespace RssTEst
             Console.WriteLine("Categories ajoutées");
         }
 
-        private static void InitSources()
+        private static void InitSources(bool isLimited)
         {
             var res = DBManager.Sources;
             var temp = new List<object[]>();
             temp.Add(new Object[] { "", "Le monde - International", 120, "http://rss.lemonde.fr/c/205/f/3050/index.rss", "2014-02-09 16:38:27.313" });
             temp.Add(new Object[] { "", "SebSauvage - shaarli", 600, "http://sebsauvage.net/links/?do=rss", "2014-02-09 16:38:27.313", });
             temp.Add(new Object[] { "", "PC INpact", 300, "http://www.pcinpact.com/rss/news.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Clubic", 300, "http://com.clubic.feedsportal.com/c/33464/f/581979/index.rss", "2014-02-09 16:38:27.313", "1.ico" });
-            //temp.Add(new Object[] { "", "CDH", 300, "http://www.comptoir-hardware.com/home.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "SMBC", 21600, "http://feeds.feedburner.com/smbc-comics/PvLb?format=xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Reflet", 21600, "http://reflets.info/feed/", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Explosm", 21600, "http://feeds.feedburner.com/Explosm", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Korben", 3600, "http://feeds.feedburner.com/asBlog-UpgradeYourMind?format=xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "DealLabs.com - Deals nouveaux", 120, "http://www.dealabs.com/rss/new.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[]
-            //{
-            //    "", "This is why i'm broke", 3600, "http://feeds.feedburner.com/ThisIsWhyImBroke?format=xml",
-            //    "2014-02-09 16:38:27.313"
-            //});
-            //temp.Add(new Object[] { "", "The big Picture", 43200, "http://feeds.boston.com/boston/bigpicture/index", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[]
-            //{
-            //    "Rss10FeedFormatter", "OatMeal", 21600, "http://feeds.feedburner.com/oatmealfeed?format=xml",
-            //    "2014-02-09 16:38:27.313"
-            //});
-            //temp.Add(new Object[] { "", "XKCD", 21600, "https://xkcd.com/rss.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Direct Matin", 120, "http://www.directmatin.fr/rss.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "Rss10FeedFormatter", "Steam", 600, "http://store.steampowered.com/feeds/news.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "V-inc", 3600, "http://widget.stagram.com/rss/n/vinc_fr/", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "google actu", 45, "http://news.google.fr/news?pz=1&cf=all&ned=fr&hl=fr&output=rss", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[]
-            //{
-            //    "", "Liberation", 120, "http://liberation.fr.feedsportal.com/c/32268/fe.ed/rss.liberation.fr/rss/9/",
-            //    "2014-02-09 16:38:27.313"
-            //});
-            //temp.Add(new Object[] { "", "Figaro", 120, "http://feeds.lefigaro.fr/c/32266/f/438191/index.rss", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[]
-            //{
-            //    "", "Nouvel Obs", 120, "http://rss.nouvelobs.com/c/32262/fe.ed/tempsreel.nouvelobs.com/rss.xml",
-            //    "2014-02-09 16:38:27.313"
-            //});
-            //temp.Add(new Object[] { "", "Rue 89", 600, "http://rue89.feedsportal.com/c/33822/f/608959/index.rss", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Le point", 120, "http://www.lepoint.fr/rss.xml", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "StackOverflow", 120, "http://stackoverflow.com/feeds", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[]
-            //{
-            //    "Rss20FeedFormatter", "Monsieur le hien", 21600, "http://www.monsieur-le-chien.fr/rss.php",
-            //    "2014-02-09 16:38:27.313"
-            //});
-            //temp.Add(new Object[] { "", "DTC", 300, "http://feeds.feedburner.com/bashfr", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Hollandais Volant", 600, "http://lehollandaisvolant.net/rss.php", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Numerama", 300, "http://www.numerama.com/rss/news.rss", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[]
-            //{
-            //    "", "NY Times - International", 120, "http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml",
-            //    "2014-02-09 16:38:27.313"
-            //});
-            //temp.Add(new Object[] { "", "Sam & max", 3600, "http://sametmax.com/feed/", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "VDM", 300, "http://feedpress.me/viedemerde", "2014-02-09 16:38:27.313" });
-            //temp.Add(new Object[] { "", "Torrent411 - Derniers Films", 150, "http://www.t411.me/rss/?cat=631", "2014-02-09 16:38:27.313" });
-
+            if (!isLimited)
+            {
+                temp.Add(new Object[]
+                {
+                    "", "Clubic", 300, "http://com.clubic.feedsportal.com/c/33464/f/581979/index.rss",
+                    "2014-02-09 16:38:27.313", "1.ico"
+                });
+                temp.Add(new Object[]
+                {"", "CDH", 300, "http://www.comptoir-hardware.com/home.xml", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "", "SMBC", 21600, "http://feeds.feedburner.com/smbc-comics/PvLb?format=xml", "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[] {"", "Reflet", 21600, "http://reflets.info/feed/", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {"", "Explosm", 21600, "http://feeds.feedburner.com/Explosm", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "", "Korben", 3600, "http://feeds.feedburner.com/asBlog-UpgradeYourMind?format=xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {
+                    "", "DealLabs.com - Deals nouveaux", 120, "http://www.dealabs.com/rss/new.xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {
+                    "", "This is why i'm broke", 3600, "http://feeds.feedburner.com/ThisIsWhyImBroke?format=xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {
+                    "", "The big Picture", 43200, "http://feeds.boston.com/boston/bigpicture/index",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {
+                    "Rss10FeedFormatter", "OatMeal", 21600, "http://feeds.feedburner.com/oatmealfeed?format=xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[] {"", "XKCD", 21600, "https://xkcd.com/rss.xml", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {"", "Direct Matin", 120, "http://www.directmatin.fr/rss.xml", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "Rss10FeedFormatter", "Steam", 600, "http://store.steampowered.com/feeds/news.xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {"", "V-inc", 3600, "http://widget.stagram.com/rss/n/vinc_fr/", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "", "google actu", 45, "http://news.google.fr/news?pz=1&cf=all&ned=fr&hl=fr&output=rss",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {
+                    "", "Liberation", 120, "http://liberation.fr.feedsportal.com/c/32268/fe.ed/rss.liberation.fr/rss/9/",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {"", "Figaro", 120, "http://feeds.lefigaro.fr/c/32266/f/438191/index.rss", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "", "Nouvel Obs", 120, "http://rss.nouvelobs.com/c/32262/fe.ed/tempsreel.nouvelobs.com/rss.xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[]
+                {
+                    "", "Rue 89", 600, "http://rue89.feedsportal.com/c/33822/f/608959/index.rss", "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[] {"", "Le point", 120, "http://www.lepoint.fr/rss.xml", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {"", "StackOverflow", 120, "http://stackoverflow.com/feeds", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "Rss20FeedFormatter", "Monsieur le hien", 21600, "http://www.monsieur-le-chien.fr/rss.php",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[] {"", "DTC", 300, "http://feeds.feedburner.com/bashfr", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {"", "Hollandais Volant", 600, "http://lehollandaisvolant.net/rss.php", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {"", "Numerama", 300, "http://www.numerama.com/rss/news.rss", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {
+                    "", "NY Times - International", 120,
+                    "http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml",
+                    "2014-02-09 16:38:27.313"
+                });
+                temp.Add(new Object[] {"", "Sam & max", 3600, "http://sametmax.com/feed/", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[] {"", "VDM", 300, "http://feedpress.me/viedemerde", "2014-02-09 16:38:27.313"});
+                temp.Add(new Object[]
+                {"", "Torrent411 - Derniers Films", 150, "http://www.t411.me/rss/?cat=631", "2014-02-09 16:38:27.313"});
+            }
             foreach (var objectse in temp)
             {
                 Source s = new Source()
