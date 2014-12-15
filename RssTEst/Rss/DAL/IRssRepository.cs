eb26e151c5fc74c39fb7c44398ffed1de7e2f3f8
Chapelin <antbichon@gmail.com>
2014-12-15 13:57:48 +0100

    Updated RssRepository interface (Interfaces collections)

--- RssTEst/Rss/DAL/IRssRepository.cs
+++ RssTEst/Rss/DAL/IRssRepository.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rss.DAL
+﻿namespace Rss.DAL
 {
+    using MongoDB.Driver;
+
+    using Rss.DTO;
+
+    /// <summary>
+    /// The RssRepository interface.
+    /// </summary>
     public interface IRssRepository
     {
+        /// <summary>
+        /// Gets the entries.
+        /// </summary>
+        IMongoCollection<FeedEntry> Entries { get; }
+
+        /// <summary>
+        /// Gets the sources.
+        /// </summary>
+        IMongoCollection<FeedSource> Sources { get; }
+
+        /// <summary>
+        /// Gets the categories.
+        /// </summary>
+        IMongoCollection<SourceCategory> Categories { get; }
+
+        /// <summary>
+        /// Gets the favicons.
+        /// </summary>
+        IMongoCollection<Favicon> Favicons { get; }
+
 
     }
 }
--- RssTEst/Rss/Services/INetDownloader.cs
+++ RssTEst/Rss/Services/INetDownloader.cs
@@ -10,5 +10,6 @@ namespace Rss.Services
         /// <param name="url">The URL.</param>
         /// <returns>The stream of the content</returns>
         Stream GetStreamFromUrl(string url);
+
     }
 }
