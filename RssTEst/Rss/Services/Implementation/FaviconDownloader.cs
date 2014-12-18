9a7b0085cdd39eda48e000b978cf5867344cfc0e
Chapelin <antbichon@gmail.com>
2014-12-18 13:27:42 +0100

    beginning FaviconDownloader

--- RssTEst/Rss/Services/Implementation/FaviconDownloader.cs
+++ RssTEst/Rss/Services/Implementation/FaviconDownloader.cs
@@ -9,7 +9,20 @@ namespace Rss.Services.Implementation
 {
     public class FaviconDownloader : IFaviconDownloader
     {
+
+        private INetDownloader netdowloader;
+
+        public FaviconDownloader(INetDownloader netdowloader)
+        {
+            this.netdowloader = netdowloader;
+        }
+
         public Favicon GetFaviconFromFeedUrl(string url)
+        {
+            List<Uri> listPotentialUris = this.GetPotentialUriForFavicon(url);
+        }
+
+        public List<Uri> GetPotentialUriForFavicon(string url)
         {
             throw new NotImplementedException();
         }
